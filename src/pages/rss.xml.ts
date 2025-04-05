736c71e77b6825671e2508bb5d1cfd04366c305a
April-Aries <luke041510@gmail.com>
2025-04-06 00:24:49 +0800

    [Fix bug] Modify rss.xml configuration

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 
-export const GET = async (context) => {
+export const GET = async (context: APIContext) => {
   const posts = await getCollection('blog');
 
   const rssContent = await rss({
     title: "AA's Blog",
     description: '',
-    site: context.site,
+    site: context.site ?? new URL('https://april-aries-github-io.vercel.app'),
     items: posts.map((post) => ({
       title: post.data.title,
       summary: post.data.summary,
